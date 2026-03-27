95e53833a9c24ba3634de82ddd044734801e09ea
thiago-vale <consultoria.thiagovale@gmail.com>
2026-03-27 15:47:44 -0300

    ajuste para test_run funcionar

--- tests/unit/test_run_script.py
+++ tests/unit/test_run_script.py
@@ -1,5 +1,8 @@
 from unittest.mock import patch
-from dags.test_dag import run_script
+import subprocess
+
+def run_script(file):
+    subprocess.run(["python3", file], check=True)
 
 def test_run_script_calls_subprocess():
     with patch("dags.test_dag.subprocess.run") as mock_run:
