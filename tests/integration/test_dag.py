381f3a2f7fddac9136309603bc1c3c528853ec2c
thiago-vale <consultoria.thiagovale@gmail.com>
2026-03-27 16:59:24 -0300

    correção do teste

--- tests/integration/test_dag.py
+++ tests/integration/test_dag.py
@@ -1,7 +1,10 @@
 from airflow.models import DagBag
 
+
 def test_dag_loaded():
-    dag_bag = DagBag()
+    dag_bag = DagBag(dag_folder="dags/", include_examples=False)
+
+    assert len(dag_bag.import_errors) == 0, f"Erros: {dag_bag.import_errors}"
 
     dag = dag_bag.get_dag(dag_id="primeira_dag")
 
@@ -10,8 +13,11 @@ def test_dag_loaded():
 
 
 def test_dag_tasks():
-    dag_bag = DagBag()
-    dag = dag_bag.get_dag("primeira_dag",)
+    dag_bag = DagBag(dag_folder="dags/", include_examples=False)
+
+    dag = dag_bag.get_dag("primeira_dag")
+
+    assert dag is not None  # 👈 importante
 
     task_ids = [task.task_id for task in dag.tasks]
 
